2e53b026a1b4b1cd5d71cc248d2271c01eb16438
kevguy <kevlai@uw.edu>
2016-02-01 09:46:59 +0800

    Sample Message

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 //Starter Project for the Reddit Clone
 var app = angular.module('reddit-clone', ['ngRoute', 'firebase']);
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/', {
+		controller: 'MainController',
+		templateUrl: 'main.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	})
+
+});
+
+app.controller('MainController', function($scope, firebase) {
+
+});
