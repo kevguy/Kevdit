caa4f38ca7b3ab381b034b9b277f4cb69802f7d5
kevguy <kevlai@uw.edu>
2016-02-01 11:59:23 +0800

    Add Simple Data

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
 //Starter Project for the Reddit Clone
 var app = angular.module('reddit-clone', ['ngRoute', 'firebase']);
 
-app.config(function($routeProvider){
-	$routeProvider
-	.when('/', {
-		controller: 'MainController',
-		templateUrl: 'main.html'
-	})
-	.otherwise({
-		redirectTo: '/'
-	})
+//The Firebase URL set as a Constant
+app.constant('fbURL', 'https://kevdit.firebaseio.com');
 
+//Creating a factory so we can use the Firebase URL
+app.factory('Posts', function ($firebase, fbURL) {
+    return $firebase(new Firebase(fbURL)).$asArray();
 });
 
-app.controller('MainController', function($scope, firebase) {
+//Configuring the route providers to redirect to the right location
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'MainController',
+            templateUrl: 'main.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+});
 
+//The Main Controller that holds everything
+app.controller('MainController', function ($scope, $firebase, Posts) {
+
+    //Set the posts we get to a global variable that can be used
+    $scope.posts = Posts;
+
+    //The function that runs when the user saves a post
+    $scope.savePost = function (post) {
+		Posts.$add({
+			name: post.name,
+			dscription: post.description,
+			url: post.url
+		})
+
+		post.name = "";
+		post.description = "";
+		post.url = "";
+
+	}
 });
